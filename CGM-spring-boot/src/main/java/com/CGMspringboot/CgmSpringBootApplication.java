ccc1d288c117ca7269d9f406840e8ddacc1ccb96
TodoranIonut <todoran.ionut96@gmail.com>
2023-04-17 21:17:25 +0300

    init cgm project

---
+++ CGM-spring-boot/src/main/java/com/CGMspringboot/CgmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.CGMspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgmSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgmSpringBootApplication.class, args);
+	}
+
+}
---
+++ CGM-spring-boot/src/test/java/com/CGMspringboot/CgmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CGMspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgmSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
